f25771a24788f925d76d6421691dcdd3499c38ea
Clément POULAIN <cpoulain@wideagency.com>
2019-01-29 17:05:31 +0100

    Error module

---
+++ src/app/core/error/error.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nd-error',
+  templateUrl: 'error.component.html'
+})
+
+export class ErrorComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ src/app/core/error/error.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { ErrorComponent } from './error.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [ErrorComponent],
+  providers: [],
+})
+export class ErrorModule { }

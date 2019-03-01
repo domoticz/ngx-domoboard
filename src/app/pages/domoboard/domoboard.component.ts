f39e42ba2d0d34df9f34582d65db991ed0f96dc7
Clément POULAIN <cpoulain@wideagency.com>
2019-03-01 10:46:20 +0100

    new package.json

--- src/app/pages/domoboard/domoboard.component.ts
+++ src/app/pages/domoboard/domoboard.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { DomoticzService } from 'src/app/core/services/domoticz.service';
+
 import { Observable } from 'rxjs';
+
+import { DomoticzService } from 'src/app/core/services/domoticz.service';
 import { LightSwitch } from 'src/app/core/models/light-switch.interface';
 
 @Component({
   selector: 'nd-domoboard',
   templateUrl: './domoboard.component.html'
 })
-
 export class DomoboardComponent implements OnInit {
 
   lightSwitches$: Observable<LightSwitch[]> = this.domoticzService.getLightSwitches();

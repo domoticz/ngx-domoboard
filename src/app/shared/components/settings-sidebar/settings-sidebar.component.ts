25818051db715b2b88c9a5fe9ceac403348c6b41
Clément POULAIN <cpoulain@wideagency.com>
2019-04-18 17:50:10 +0200

    settings sidebar animation

--- src/app/shared/components/settings-sidebar/settings-sidebar.component.ts
+++ src/app/shared/components/settings-sidebar/settings-sidebar.component.ts
@@ -1,17 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'nd-settings-sidebar',
   template: `
     <div class="sidebar-container">
-      <nd-toggle-settings-button class="settings-button"></nd-toggle-settings-button>
-      <div class="sidebar-content"></div>
+      <nd-toggle-settings-button class="settings-button" (slideIn)="show()"
+        [animationState]="animationState" (slideOut)="hide()">
+      </nd-toggle-settings-button>
+      <div class="settings-container {{ animationState }}">
+        <div *ngIf="showContent" class="sidebar-content"></div>
+      </div>
     </div>
   `,
-  styleUrls: ['./settings-sidebar.component.scss']
+  styleUrls: ['./settings-sidebar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SettingsSidebarComponent implements OnInit {
-  constructor() { }
+
+  animationState = 'out';
+
+  showContent: boolean;
+
+  constructor(private cd: ChangeDetectorRef) { }
 
   ngOnInit() { }
+
+  show() {
+    this.animationState = 'in';
+    this.showContent = true;
+  }
+
+  hide() {
+    this.animationState = 'out';
+    // Wait animation duration to destroy content.
+    setTimeout(() => {
+      this.showContent = false;
+      this.cd.detectChanges();
+    }, 450);
+  }
+
 }
--- src/app/shared/components/settings-sidebar/toggle-settings-button/toggle-settings-button.component.ts
+++ src/app/shared/components/settings-sidebar/toggle-settings-button/toggle-settings-button.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'nd-toggle-settings-button',
   template: `
-    <button class="toggle-settings">
-      <i class="nb-gear"></i>
+    <button class="toggle-settings" (click)="animationState === 'in' ? slideOut.emit() : slideIn.emit()">
+      <nd-svg-icon class="nd-gear" [ngClass]="{ 'pulse': animationState === 'out' }"
+        [name]="'nd-gear'">
+      </nd-svg-icon>
     </button>
   `,
-  styleUrls: ['./toggle-settings-button.component.scss']
+  styleUrls: ['./toggle-settings-button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ToggleSettingsButtonComponent implements OnInit {
-  constructor() { }
+export class ToggleSettingsButtonComponent {
 
-  ngOnInit() { }
+  @Input() animationState: string;
+
+  @Output() slideIn = new EventEmitter();
+
+  @Output() slideOut = new EventEmitter();
+
 }

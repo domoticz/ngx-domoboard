b14fb6786e0195fa4f166d3e7f5b84fdb2f457dc
6l3m <clmpoulain@gmail.com>
2019-04-23 22:01:11 +0200

    base url tsconfig webpack plugin

---
+++ cypress/plugins/cy-ts-preprocessor.js
@@ -0,0 +1,33 @@
+const wp = require('@cypress/webpack-preprocessor')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
+const webpackOptions = {
+  resolve: {
+    extensions: ['.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin({ configFile: "../tsconfig.json" })]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: ['ts-loader', 'angular2-template-loader'],
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.(html|css)$/,
+        loader: 'raw-loader',
+        exclude: /\.async\.(html|css)$/
+      },
+      {
+        test: /\.async\.(html|css)$/,
+        loaders: ['file?name=[name].[hash].[ext]', 'extract']
+      }
+    ]
+  }
+}
+
+const options = {
+  webpackOptions
+}
+
+module.exports = wp(options)

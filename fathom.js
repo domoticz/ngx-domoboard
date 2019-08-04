b5ae841168f20fc5d3639624500d2cef9444c32c
6l3m <clmpoulain@gmail.com>
2019-08-04 15:12:08 +0200

    chg fathom url

--- fathom.js
+++ fathom.js
@@ -6,6 +6,6 @@
 	m=f.getElementsByTagName('script')[0];
 	o.async=1; o.src=t; o.id='fathom-script';
 	m.parentNode.insertBefore(o,m)
-})(document, window, '//nd-api.duckdns.org/tracker.js', 'fathom');
+})(document, window, 'https://nd-api.duckdns.org:5035/fathom/tracker.js', 'fathom');
 fathom('set', 'siteId', 'KXECY');
 fathom('trackPageview');

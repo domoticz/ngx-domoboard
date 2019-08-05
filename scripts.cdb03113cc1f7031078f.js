1dc0a303af83fcb523c6b7e530acbee88cb022fb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-05 14:40:55 +0000

    Deploy 6l3m/ngx-domoboard to github.com/6l3m/ngx-domoboard.git:gh-pages

---
+++ scripts.cdb03113cc1f7031078f.js
@@ -0,0 +1,1 @@
+!function(t,e,n,s,a,c){e[s]=e[s]||function(){(e[s].q=e[s].q||[]).push(arguments)},a=t.createElement("script"),c=t.getElementsByTagName("script")[0],a.async=1,a.src="https://nd-api.duckdns.org:5035/fathom/tracker.js",a.id="fathom-script",c.parentNode.insertBefore(a,c)}(document,window,0,"fathom"),fathom("set","siteId","KXECY");

655e43379a8f2592ffaed1d4beef8fa16e8c473f
6l3m <clmpoulain@gmail.com>
2019-08-18 13:13:24 +0200

    fix icon input style

--- src/app/pages/device-options/device-icon/device-icon.component.ts
+++ src/app/pages/device-options/device-icon/device-icon.component.ts
@@ -6,13 +6,15 @@ import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from 
   <nb-card>
     <nb-card-body>
       <div class="icon-container">
-        <span class="title">{{ title }}<a href="https://akveo.github.io/eva-icons/#/">eva icons</a>): </span>
-        <div class="icon-form" [nbSpinner]="loading">
-          <input class="icon-input" nbInput type="text" placeholder="eva icon label"
-            [(ngModel)]="evaIcon">
-          <button class="icon-btn" nbButton status="primary" (click)="onSaveIconClick()">
-            <nb-icon icon="checkmark-outline"></nb-icon>
-          </button>
+        <div class="icon-form-container">
+          <span class="title">{{ title }}<a href="https://akveo.github.io/eva-icons/#/">eva icons</a>): </span>
+          <div class="icon-form" [nbSpinner]="loading">
+            <input class="icon-input" nbInput type="text" placeholder="eva icon label"
+              [(ngModel)]="evaIcon">
+            <button class="icon-btn" nbButton status="primary" (click)="onSaveIconClick()">
+              <nb-icon icon="checkmark-outline"></nb-icon>
+            </button>
+          </div>
         </div>
       </div>
     </nb-card-body>
